0097950fd9e0143e105fd4275b6ae60e800317e6
Kate Drennen <k8ted11@gmail.com>
2018-02-07 02:40:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,11 +35,11 @@ def turn(board)
   user_input = gets
   # convert user
   #  input to integer call input_to_index
-  input_to_index(user_input)
+  index = user_input
 
 #   # check to see if user_input is a valid move
- if valid_move?(input_to_index) != position_taken
-   puts display_board(board)
+ if valid_move?(board, index)
+   puts move(board, index)
 #  elseif user_input != valid_move?
 #  turn(board)
 # #   true
